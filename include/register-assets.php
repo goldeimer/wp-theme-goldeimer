048f46a4c25cfb6e4a73b04d15d64014333f8f17
Johannes Pilkahn <johannes@goldeimer.de>
2020-11-18 15:32:28 +0100

    refactor(wp-util): saner class hierarchy, separation of concerns for registering & enqueueing

--- include/register-assets.php
+++ include/register-assets.php
@@ -4,5 +4,10 @@ namespace Goldeimer\WordPress\WpTheme;
 
 use Goldeimer\WordPress\WpUtil\WebpackAssetLoader;
 
-$loader = new WebpackAssetLoader(GOLDEIMER_THEME_ABSPATH);
+$loader = new WebpackAssetLoader(
+    \get_stylesheet_directory_uri(),
+    GOLDEIMER_THEME_ABSPATH
+);
+
 $loader->register();
+$loader->enqueue();
